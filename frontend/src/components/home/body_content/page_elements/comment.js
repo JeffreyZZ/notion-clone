02857f6f6d88f45327283bc2af6bb6ca8397f67f
Jeffrey Zhou <jingzhou@microsoft.com>
2023-06-18 11:32:59 -0700

    buttons

--- frontend/src/components/home/body_content/page_elements/comment.js
+++ frontend/src/components/home/body_content/page_elements/comment.js
@@ -1,6 +1,8 @@
 import React from 'react';
-import { Box, Typography, Avatar, Paper } from '@mui/material';
+import { Box, Typography, Avatar, Paper, IconButton } from '@mui/material';
 import { makeStyles } from '@material-ui/core/styles';
+import FileCopyIcon from '@mui/icons-material/FileCopy';
+import PasteIcon from '@mui/icons-material/ContentPaste';
 
 const useStyles = makeStyles((theme) => ({
     commentContainer: {
@@ -18,24 +20,54 @@ const useStyles = makeStyles((theme) => ({
         fontSize: '0.8rem',
         color: theme.palette.text.secondary,
     },
+    container: {
+        position: 'relative',
+    },
+    buttonsContainer: {
+        position: 'absolute',
+        top: theme.spacing(1),
+        right: theme.spacing(1),
+        display: 'flex',
+        alignItems: 'center',
+    },
+    button: {
+        marginLeft: theme.spacing(1),
+    },
 }));
 
 const Comment = ({ comment, unread }) => {
     const classes = useStyles({ unread: unread }); // Pass the unread status as a prop to useStyles
+    const handleCopy = () => {
+        // Handle copy logic here
+    };
+
+    const handlePaste = () => {
+        // Handle paste logic here
+    };
 
     return (
-        <Paper className={classes.commentContainer} elevation={2}>
-            <Box display="flex" alignItems="center" marginBottom={1}>
-                <Avatar alt={comment.author} />
-                <Typography variant="subtitle1" className={classes.commentAuthor}>
-                    {comment.author}
-                </Typography>
-                <Typography variant="caption" className={classes.commentDate}>
-                    {comment.date}
-                </Typography>
-            </Box>
-            <Typography variant="body1">{comment.body}</Typography>
-        </Paper>
+        <Box className={classes.container}>
+            <div className={classes.buttonsContainer}>
+                <IconButton className={classes.button} aria-label="Copy" onClick={handleCopy}>
+                    <FileCopyIcon />
+                </IconButton>
+                <IconButton className={classes.button} aria-label="Paste" onClick={handlePaste}>
+                    <PasteIcon />
+                </IconButton>
+            </div>
+            <Paper className={classes.commentContainer} elevation={2}>
+                <Box display="flex" alignItems="center" marginBottom={1}>
+                    <Avatar alt={comment.author} />
+                    <Typography variant="subtitle1" className={classes.commentAuthor}>
+                        {comment.author}
+                    </Typography>
+                    <Typography variant="caption" className={classes.commentDate}>
+                        {comment.date}
+                    </Typography>
+                </Box>
+                <Typography variant="body1">{comment.body}</Typography>
+            </Paper>
+        </Box>
     );
 };
 
