9b9c96a69d3b82bcff57f2d10376771db68cc8a7
Jeffrey Zhou <jingzhou@microsoft.com>
2023-06-20 10:33:34 -0700

    Debug React

--- webpack.config.js
+++ webpack.config.js
@@ -1,21 +1,23 @@
 module.exports = {
-    module: {
-        rules: [
-            {
-                test: /\.js$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                }
-            },
-            {
-                test: /\.(png|jpe?g|gif)$/i,
-                use: [
-                  {
-                    loader: 'file-loader',
-                  },
-                ],
-              },
-        ]
-    }
+  // for debugging
+  devtool: "eval-source-map",
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: "babel-loader",
+        }
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
+    ]
+  }
 }
