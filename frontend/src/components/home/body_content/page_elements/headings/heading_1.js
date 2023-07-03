ab3f5d6a4cb66edd1125edf92151d9db9afc9356
Jeffrey Zhou <jingzhou@microsoft.com>
2023-07-02 21:02:29 -0700

    Fix head_1 and _2 issues

--- frontend/src/components/home/body_content/page_elements/headings/heading_1.js
+++ frontend/src/components/home/body_content/page_elements/headings/heading_1.js
@@ -10,20 +10,28 @@ function Heading_1(props) {
     // In case there is an element whose type is Heading_1 but there is no heading 1...
     if ((props.page_element.heading_1).length === 0) return null
 
+    const [initialValue, setInitialValue] = useState(props.page_element.heading_1[0].heading_text);
     const [heading_1, set_heading_1] = useState(props.page_element.heading_1[0].heading_text)
+
+    const handleBlur = () => {
+        if (heading_1 !== initialValue) {
+            props.edit_H1(props.page_element.heading_1[0].id, heading_1);
+            setInitialValue(heading_1);
+        }
+    };
 
     return (
         <div className="side-by-side e_container">
-            <input 
+            <input
                 ref={myRef}
-                autoComplete="off" 
-                onChange={(e) => set_heading_1(e.target.value)} 
-                name="heading_1" 
+                autoComplete="off"
+                onChange={(e) => set_heading_1(e.target.value)}
+                name="heading_1"
                 className={`user_input heading_1 ${props.page_element.color}`}
-                placeholder="Heading 1" 
-                value={heading_1} 
-                onBlur={()=> props.edit_H1(props.page_element.heading_1[0].id, heading_1)} 
-                style={{ opacity: props.snapshot.isDragging? '0.5': '1' }}/>
+                placeholder="Heading 1"
+                value={heading_1}
+                onBlur={handleBlur}
+                style={{ opacity: props.snapshot.isDragging ? '0.5' : '1' }} />
         </div>
     )
 }
--- frontend/src/components/home/body_content/page_elements/headings/heading_2.js
+++ frontend/src/components/home/body_content/page_elements/headings/heading_2.js
@@ -6,26 +6,32 @@ function Heading_2(props) {
     useEffect(() => {
         myRef.current.focus({ preventScroll: true });
     }, [])
-    
+
     // In case there is an element whose type is Heading_2 but there is no heading 2...
     if ((props.page_element.heading_2).length === 0) return null
 
+    const [initialValue, setInitialValue] = useState(props.page_element.heading_2[0].heading_text);
     const [heading_2, set_heading_2] = useState(props.page_element.heading_2[0].heading_text)
+
+    const handleBlur = () => {
+        if (heading_2 !== initialValue) {
+            props.edit_H2(props.page_element.heading_2[0].id, heading_2);
+            setInitialValue(heading_2);
+        }
+    };
 
     return (
         <div className="side-by-side e_container">
-            <input 
+            <input
                 ref={myRef}
-                autoComplete="off" 
-                onChange={(e) => set_heading_2(e.target.value)} 
-                name="heading_2" 
-                className={`user_input heading_2 ${props.page_element.color}`} 
-                placeholder="Heading 2" 
-                value={heading_2} 
-                onBlur={()=> props.edit_H2(props.page_element.heading_2[0].id, heading_2)} 
-                style={{
-                    opacity: props.snapshot.isDragging? '0.5': '1'
-                }}/>
+                autoComplete="off"
+                onChange={(e) => set_heading_2(e.target.value)}
+                name="heading_2"
+                className={`user_input heading_2 ${props.page_element.color}`}
+                placeholder="Heading 2"
+                value={heading_2}
+                onBlur={handleBlur}
+                style={{ opacity: props.snapshot.isDragging ? '0.5' : '1' }} />
         </div>
     )
 }
